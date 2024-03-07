d4b56406bbe211d8c34cafd051c9d702a3c97a48
James Cowans <Jamescowans@hotmail.com>
2024-03-07 21:25:32 +0000

    install pillow and gunicor ready for first deploy

--- restaurant/models.py
+++ restaurant/models.py
@@ -22,7 +22,7 @@ class Option(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     image= models.ImageField(upload_to='options', blank=True)
-    description - models.TextField(blank=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
