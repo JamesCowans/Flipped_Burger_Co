4da67f9238fb54e48f9a2b8d746c621dc21aabc5
James Cowans <Jamescowans@hotmail.com>
2024-03-26 20:57:44 +0000

    update backet page

--- orders/forms.py
+++ orders/forms.py
@@ -5,4 +5,4 @@ from .models import Order
 class OrderPlacementForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'town', 'post_code']
+        fields = ['first_name', 'last_name', 'email', 'address', 'town', 'post_code', 'comments']
