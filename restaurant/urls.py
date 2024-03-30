00618d677d21a3548d60a19e47c1d3f10cd07a6a
James Cowans <Jamescowans@hotmail.com>
2024-03-30 10:21:52 +0000

    end of test, issue is with new microsoft edge browser

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,8 +5,7 @@ app_name = 'restaurant'
 
 urlpatterns = [
     path('', views.option_list, name = 'option_list'),
-    path('<slug:category_slug>/', views.option_list,
-         name = 'option_list_by_category'),
+    path('<slug:category_slug>/', views.option_list, name = 'option_list_by_category'),
     path('<int:id>/<slug:slug>/', views.option_detail, name='option_detail'),
 ]
 
