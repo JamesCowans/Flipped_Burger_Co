2d6e8092b6278087624bc72ce61cacf353afe60c
James Cowans <Jamescowans@hotmail.com>
2024-07-07 17:59:35 +0000

    remove alt tags error

--- restaurant/models.py
+++ restaurant/models.py
@@ -32,7 +32,6 @@ class Option(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     image = CloudinaryField('image', default='static/img/awaiting_image.png')
-    alt_tag = models.Charfield()
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
