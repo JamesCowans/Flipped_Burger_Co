7040d2207cdae899a944c45916461d2a0e4bba47
James Cowans <Jamescowans@hotmail.com>
2024-07-09 21:06:38 +0000

    reverse changes made to basket

--- basket/views.py
+++ basket/views.py
@@ -6,65 +6,43 @@ from basket.forms import BasketAddOptionForm
 
 
 
+
 # Create your views here.
 
-# @require_POST
-# def basket_add(request, option_id):
-#     basket = Basket(request)
-#     option = get_object_or_404(Option, id=option_id)
-#     form = BasketAddOptionForm(request.POST)
-#     if form.is_valid():
-#         cd = form.cleaned_data
-#         basket.add(option=option, quantity = cd['quantity'], override_quantity = cd['override'])
-#     return redirect('basket:basket_detail')
-
-
-
-def basket_detail(request):
-    
-    return render(request, 'basket/detail.html')
-
-
-
-def basket_add(request):
-    
+@require_POST
+def basket_add(request, option_id):
     basket = Basket(request)
-    
-    if request.POST.get('action') == 'POST':
-        
-        option.id = int(request.POST.get('option_id'))
-        
-        option = get_object_or_404(Option, id=option_id)
-        
-        basket.add(option=option)
-        
-        
-def basket_remove(request):
-    
-    pass
+    option = get_object_or_404(Option, id=option_id)
+    form = BasketAddOptionForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        basket.add(option=option, quantity = cd['quantity'], override_quantity = cd['override'])
+    return redirect('basket:basket_detail')
+
+
 
 def basket_update(request):
     
     pass
-
         
 
 
 
-# @require_POST
-# def basket_remove(request, option_id):
-#     basket = Basket(request)
-#     option = get_object_or_404(Option, id=option_id)
-#     basket.remove(option)
-#     return redirect('basket:basket_detail')
+
+@require_POST
+def basket_remove(request, option_id):
+    basket = Basket(request)
+    option = get_object_or_404(Option, id=option_id)
+    basket.remove(option)
+    return redirect('basket:basket_detail')
 
 
 
-# def basket_detail(request):
-#     basket = Basket(request)
-#     # for item in basket:
-#     #     item['update_quantity_form'] = BasketAddOptionForm(initial={'quantity': item['quantity'], 'override': True})
-#     return render(request, 'basket/detail.html', {'basket' : basket})
+def basket_detail(request):
+    basket = Basket(request)
+    for item in basket:
+        item['update_quantity_form'] = BasketAddOptionForm(initial={'quantity': item['quantity'], 'override': True})
+    return render(request, 'basket/detail.html', {'basket' : basket})
 
 
 
